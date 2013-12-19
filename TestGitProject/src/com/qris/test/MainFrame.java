8300308d0532ff264d363ddeaa85218823e1c77a
m.vanturnhout <m.vanturnhout@LAPTOPMJPJT.exxellence.local>
2013-12-19 14:22:41 +0100

    a new menu

--- TestGitProject/src/com/qris/test/MainFrame.java
+++ TestGitProject/src/com/qris/test/MainFrame.java
@@ -3,6 +3,9 @@ package com.qris.test;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Choice;
+import java.awt.BorderLayout;
+import java.awt.TextArea;
 
 public class MainFrame {
 
@@ -38,6 +41,15 @@ public class MainFrame {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Choice choice = new Choice();
+		frame.getContentPane().add(choice, BorderLayout.NORTH);
+		
+		TextArea textArea = new TextArea();
+		frame.getContentPane().add(textArea, BorderLayout.WEST);
+		
+		TextArea textArea_1 = new TextArea();
+		frame.getContentPane().add(textArea_1, BorderLayout.SOUTH);
 	}
 
 }
